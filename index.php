cad11b922d08845d8cec6122e5a2835a5718a71e
filippogiovannoni <filippogiovannonii@gmail.com>
2024-04-05 12:10:27 +0200

    selected and printed in page students born in 1990

--- index.php
+++ index.php
@@ -18,10 +18,44 @@ if ($connection && $connection->connect_error) {
 var_dump($connection);
 
 
-$sql = "SELECT * FROM `degrees`";
+$students_1990 = "SELECT * FROM `students` WHERE YEAR(`date_of_birth`) = 1990;";
 
-var_dump($sql);
+var_dump($students_1990);
 
-$result = $connection->query($sql);
+$result_1990 = $connection->query($students_1990);
 
-var_dump($result);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Students</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+</head>
+
+<body>
+    <h1>Students</h1>
+    <?php while ($row = $result_1990->fetch_assoc()) :
+        ['name' => $name, 'surname' => $surname, 'date_of_birth' => $date_of_birth] = $row; ?>
+
+        <div>
+            <span><?= $name ?></span>
+            <span><?= $surname ?></span>
+            <span><?= $date_of_birth ?></span>
+            <hr>
+        </div>
+
+
+
+
+    <?php endwhile; ?>
+
+
+</body>
+
+</html>
